8824909d066ce816271c6ff9eb359d31d046c553
Dadyze <adin.jahic@gmail.com>
2021-04-29 16:44:01 +0200

    Final

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const logo = document.getElementById("logo");
+const navLinks = document.getElementById("navlinks");
+
+logo.addEventListener('click',()=>{
+  navLinks.classList.toggle("open"); 
+});
+
+
+
